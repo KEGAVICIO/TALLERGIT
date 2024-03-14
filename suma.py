b7da1cd877feab13af108f0b57dafa2daf19f5ac
21Hell <76609684+21Hell@users.noreply.github.com>
2024-03-14 13:09:21 -0600

    testings and venv

--- suma.py
+++ suma.py
@@ -2,8 +2,13 @@
 num1 = float(input("Ingresa el primer número: "))
 num2 = float(input("Ingresa el segundo número: "))
 
-# Sumar los dos números
-suma = num1 + num2
+# Definir la función suma
+def suma(num1, num2):
+    return num1 + num2 # No need to assign the result to a variable inside the function
+
+# Sumar los dos números usando la función suma
+resultado = suma(num1, num2) # Store the result in a variable
 
 # Imprimir el resultado
-print("La suma de ", num1, " y ", num2, "es:", suma)
+print("La suma de", num1, "y", num2, "es:", resultado)
+
---
+++ testing.py
@@ -0,0 +1,6 @@
+import suma as sum
+
+def test_suma():
+    print(sum.suma(2, 3) == 5)
+    assert sum.suma(2, 3) == 5
+
