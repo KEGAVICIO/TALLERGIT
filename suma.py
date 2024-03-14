321d497d62ad714d5bcc58613d56ebcfae9e6392
BrianEmmanuel1 <tprog.brian.hernandez@gmail.com>
2024-03-14 13:54:22 -0600

    test_suma

--- suma.py
+++ suma.py
@@ -1,11 +1,10 @@
-# Solicitar al usuario que ingrese dos números
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
+
 
 # Definir la función suma
 def suma(num1, num2):
     return num1 + num2 # No need to assign the result to a variable inside the function
-
+num2 = 10
+num1 = 5
 # Sumar los dos números usando la función suma
 resultado = suma(num1, num2) # Store the result in a variable
 
